c90e893bba1eace53c1da140cb7a421044965238
DEBNATH SINHABABU <debnathsinhababu2017@gmail.com>
2024-07-22 20:21:45 +0530

    api call

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
+import Section from './components/Section/Section';
 function App() {
   return (
     <div className="App">
      <Navbar/>
      <Hero/>
+     <Section type='new'/>
+     <Section type='top'/>
     </div>
   );
 }
